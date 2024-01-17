85ff5b23a6f954bedf6b4d15e407a7a3b1b0820d
Helge Sverre <helge.sverre@gmail.com>
2024-01-17 01:40:35 +0100

    wip

--- app/Commands/RunSimpleAgent.php
+++ app/Commands/RunSimpleAgent.php
@@ -6,12 +6,11 @@ use App\Agent\Hooks;
 use App\Agent\SimpleAgent;
 use App\Tools\BrowseWebsiteTool;
 use App\Tools\ReadFileTool;
+use App\Tools\RunCommandTool;
 use App\Tools\SearchWebTool;
 use App\Tools\WriteFileTool;
 use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
-
-use function Laravel\Prompts\alert;
 use function Laravel\Prompts\intro;
 use function Laravel\Prompts\note;
 use function Laravel\Prompts\warning;
@@ -25,17 +24,21 @@ class RunSimpleAgent extends Command
 
         $task = $this->argument('task');
 
-        if (! $task) {
+        if (!$task) {
             $task = $this->ask('What do you want to do?');
         }
 
+        $this->renderTextboxWithHeader('Task', $task);
+        die;
+
         $hooks = new Hooks([
-            'start' => fn ($task) => alert("Task: $task"),
-            'iteration' => fn ($iteration) => intro("Step: {$iteration}"),
-            'tool_execution' => fn ($tool, $args) => $this->table(['Tool', ...array_keys($args)], [[$tool, ...array_values($args)]]),
-            'thought' => fn ($thought) => \Laravel\Prompts\info("Thought:\n".wordwrap($thought, 80)),
-            'observation' => fn ($observation) => warning('Observation: '.Str::limit(wordwrap($observation, 80), 80 * 10)),
-            'final_answer' => fn ($finalAnswer) => note(wordwrap($finalAnswer, 80)),
+            'start' => fn($task) => $this->renderTextboxWithHeader('Task', $task),
+            'iteration' => fn($iteration) => intro("Step: {$iteration}"),
+            'tool_execution' => fn($tool, $args) => $this->table(['Tool', ...array_keys($args)], [[$tool, ...array_values($args)]]),
+            'thought' => fn($thought) => \Laravel\Prompts\info("Thought:\n" . wordwrap($thought, 80)),
+            'observation' => fn($observation) => warning('Observation: ' . Str::limit(wordwrap($observation, 80), 80 * 10)),
+            'final_answer' => fn($finalAnswer) => note(wordwrap($finalAnswer, 80)),
+            'next_step' => fn($step) => dump($step),
         ]);
 
         $agent = new SimpleAgent(
@@ -44,6 +47,7 @@ class RunSimpleAgent extends Command
                 new WriteFileTool(base_path('agent_output')),
                 new SearchWebTool(),
                 new BrowseWebsiteTool(),
+                new RunCommandTool(),
             ],
             goal: 'Respond to the human as helpfully and accurately as possible. The human will ask you to do things, and you should do them.',
             hooks: $hooks,
@@ -52,4 +56,13 @@ class RunSimpleAgent extends Command
         $finalResponse = $agent->run($task);
 
     }
+
+    public function renderTextboxWithHeader(string $header, string $text, int $maxWidth = 80): void
+    {
+        // TODO: With box around all sides
+
+
+
+    }
+
 }
